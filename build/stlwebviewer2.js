ebec7d188128c26a0c8e211615d9fb56a11a4b79
Brent Yi <yibrenth@gmail.com>
2019-08-31 18:28:11 -0700

    add fullscreen mode, code refactor

--- build/stlwebviewer2.js
+++ build/stlwebviewer2.js
@@ -1404,165 +1404,201 @@ THREE.STLLoader.prototype = {
  * brentyi@berkeley.edu
  */
 
-$(function() {
-    $(".stlwv2-model").each(function() {
-        $container = $(this);
-        THREE.STLWebViewer2($container.data("model-url"), $container);
-    });
-});
-
-THREE.STLWebViewer2 = function(modelURL, $container, showBoundingBox, loadedCallback) {
-    if (!Detector.webgl) Detector.addGetWebGLMessage();
-    var camera, controls, cameraTarget, scene, renderer, pointLight;
-
-    if($container == undefined) {
-        $container = $('body');
-    }
-
-    camera = new THREE.PerspectiveCamera(40, $container.width() / $container.height(), 1, 15000);
-    camera.position.set(50, 50, 50);
-    cameraTarget = new THREE.Vector3();
-    controls = new THREE.OrbitControls(camera, $container.get(0));
-    controls.target = cameraTarget;
-    controls.addEventListener('change', render);
-    controls.enableDamping = true;
-    controls.enableKeys = false;
-    controls.rotateSpeed = 0.15;
-    controls.dampingFactor = 0.125;
-    controls.enableZoom = true;
-    controls.autoRotate = true;
-    controls.autoRotateSpeed = 0.25;
-    controls.autoRotateDelay = 5000;
-    scene = new THREE.Scene();
-
-    var loader = new THREE.STLLoader();
-
-    pointLight = new THREE.PointLight( 0xdddddd, 0.75, 0);
-
-    fileSize = 0;
-
-    if ($container.children('.percent').length == 0)
-        $container.append('<div class="percent"></div>');
-    onProgress = function(event) {
-        console.log(event.loaded + "/" + event.total);
-        fileSize = event.total;
-        $container.children('.percent').text(Math.floor(event.loaded / event.total * 100.0) + "%");
-    };
-
-    loader.load(modelURL, function(geometry) {
-        var material = new THREE.MeshPhongMaterial({
-            color: 0xf7f8ff,
-            specular: 0x111111,
-            shininess: 0,
-            wireframe: false,
-            polygonOffset: true,
-            polygonOffsetFactor: 1,
-            polygonOffsetUnits: 1,
-            transparent: true,
-            opacity: 0.85
+(() => {
+    $(() => {
+        $('.stlwv2-model').each(function() {
+            let $container = $(this);
+            let modelUrl = $container.data('model-url');
+            STLWebViewer2(modelUrl, $container);
         });
+    });
 
-        var mesh = new THREE.Mesh(geometry, material);
+    let viewerCount = 0;
+    let STLWebViewer2 = (modelUrl, $container, showBoundingBox, loadedCallback) => {
+        // Check for WebGl support
+        if (!Detector.webgl) Detector.addGetWebGLMessage();
 
-        mesh.position.set(0, 0, 0);
-        mesh.castShadow = false;
-        mesh.receiveShadow = false;
-        scene.add(mesh);
-
-        var edges = new THREE.EdgesGeometry(geometry, 29);
-        var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({
-            color: 0x666666
-        }));
-        scene.add(line);
-
-        geometry.computeBoundingSphere();
-        geometry.computeBoundingBox();
-        cameraTarget.copy(geometry.boundingSphere.center);
-
-        var r = geometry.boundingSphere.radius;
-        controls.maxDistance = r * 10;
-
-        pointLight.position.set(0, r, 0);
-        camera.position.set(
-            r * 1.5 + cameraTarget.x,
-            r * 1.5 + cameraTarget.y,
-            r * 1.5 + cameraTarget.z
-        );
-
-        if (showBoundingBox) {
-            var box = new THREE.BoundingBoxHelper(mesh, 0xff7777);
-            scene.add(box);
+        // If no container is defined, use body
+        if($container == undefined) {
+            $container = $('body');
         }
 
-        $container.addClass('loaded');
-        loadedCallback && loadedCallback({
-            volume: calculateVolume(mesh),
-            width: Math.abs(mesh.geometry.boundingBox.max.x - mesh.geometry.boundingBox.min.x),
-            height: Math.abs(mesh.geometry.boundingBox.max.y - mesh.geometry.boundingBox.min.y),
-            length: Math.abs(mesh.geometry.boundingBox.max.z - mesh.geometry.boundingBox.min.z),
-            fileSize: fileSize
+        // Build out viewer DOM elements
+        expandCheckboxId = 'stlwv2-expand-' + viewerCount;
+        $container.append('\
+            <input class="expand-checkbox" id="' + expandCheckboxId + '" type="checkbox"></input>\
+            <div class="inner">\
+                <div class="percent"></div>\
+                <label class="fullscreen-on" for="' + expandCheckboxId + '">fullscreen</label>\
+                <label class="fullscreen-off" for="' + expandCheckboxId + '">close</label>\
+            </div>\
+        ');
+        let $innerContainer = $container.children('.inner');
+
+        // Start building our threejs scene
+        let scene = new THREE.Scene();
+
+        // Camera
+        let camera = new THREE.PerspectiveCamera(40, $innerContainer.width() / $innerContainer.height(), 1, 15000);
+        camera.position.set(50, 50, 50);
+        let cameraTarget = new THREE.Vector3();
+
+        // Orbit controls
+        let controls = new THREE.OrbitControls(camera, $innerContainer.get(0));
+        controls.target = cameraTarget;
+        controls.addEventListener('change', render);
+        controls.enableDamping = true;
+        controls.enableKeys = false;
+        controls.rotateSpeed = 0.15;
+        controls.dampingFactor = 0.125;
+        controls.enableZoom = true;
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 0.25;
+        controls.autoRotateDelay = 5000;
+
+        // Lights: hemisphere light attached to the world
+        let hemisphereLight = new THREE.HemisphereLight(0x999999, 0x555555);
+        scene.add(hemisphereLight);
+
+        // Lights: point light attached to the camera
+        let pointLight = new THREE.PointLight( 0xdddddd, 0.75, 0);
+        camera.add(pointLight);
+        scene.add(camera);
+
+        // Load STL file and add to scene
+        let loader = new THREE.STLLoader();
+        let fileSize = 0;
+        let onProgress = (event) => {
+            // Progress callback -- (for % loaded indicator)
+            console.log("Loading " + modelUrl + ": " + event.loaded + "/" + event.total);
+            fileSize = event.total;
+            $innerContainer.children('.percent').text(Math.floor(event.loaded / event.total * 100.0) + "%");
+        };
+        let onLoaded = (geometry) => {
+            // Callback for when our mesh has been fully loaded
+
+            // Define (shaded) mesh and add to scene
+            let material = new THREE.MeshPhongMaterial({
+                color: 0xf7f8ff,
+                specular: 0x111111,
+                shininess: 0,
+                wireframe: false,
+                polygonOffset: true,
+                polygonOffsetFactor: 1,
+                polygonOffsetUnits: 1,
+                transparent: true,
+                opacity: 0.85
+            });
+
+            let mesh = new THREE.Mesh(geometry, material);
+
+            mesh.position.set(0, 0, 0);
+            mesh.castShadow = false;
+            mesh.receiveShadow = false;
+            scene.add(mesh);
+
+            // Render model edges
+            let edges = new THREE.EdgesGeometry(geometry, 29);
+            let line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({
+                color: 0x666666
+            }));
+            scene.add(line);
+
+            // Update model bounding box and sphere
+            geometry.computeBoundingSphere();
+            geometry.computeBoundingBox();
+            cameraTarget.copy(geometry.boundingSphere.center);
+
+            // Set light, camera, and orbit control parameters based on model size
+            let r = geometry.boundingSphere.radius;
+            controls.maxDistance = r * 10;
+            pointLight.position.set(0, r, 0);
+            camera.position.set(
+                r * 1.5 + cameraTarget.x,
+                r * 1.5 + cameraTarget.y,
+                r * 1.5 + cameraTarget.z
+            );
+
+            // Render a bounding box
+            if (showBoundingBox) {
+                let box = new THREE.BoundingBoxHelper(mesh, 0xff7777);
+                scene.add(box);
+            }
+
+            // Done!
+            $innerContainer.addClass('loaded');
+            loadedCallback && loadedCallback({
+                volume: calculateVolume(mesh),
+                width: Math.abs(mesh.geometry.boundingBox.max.x - mesh.geometry.boundingBox.min.x),
+                height: Math.abs(mesh.geometry.boundingBox.max.y - mesh.geometry.boundingBox.min.y),
+                length: Math.abs(mesh.geometry.boundingBox.max.z - mesh.geometry.boundingBox.min.z),
+                fileSize: fileSize
+            });
+        }
+        loader.load(modelUrl, onLoaded, onProgress);
+
+        // Rendering!
+        let renderer = new THREE.WebGLRenderer({
+            antialias: false
         });
-    }, onProgress);
+        renderer.setClearColor(0xffffff);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize($innerContainer.width(), $innerContainer.height());
+        renderer.gammaInput = true;
+        renderer.gammaOutput = true;
+        renderer.shadowMap.enabled = true;
+        $innerContainer.append(renderer.domElement);
 
-    // Lights
-    scene.add(new THREE.HemisphereLight(0x999999, 0x555555));
-    camera.add(pointLight);
-    scene.add(camera);
+        animate();
 
-    renderer = new THREE.WebGLRenderer({
-        antialias: false
-    });
-    renderer.setClearColor(0xffffff);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize($container.width(), $container.height());
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
-    renderer.shadowMap.enabled = true;
-    $container.append(renderer.domElement);
+        function animate() {
+            camera.aspect = $innerContainer.width() / $innerContainer.height();
+            camera.updateProjectionMatrix();
+            renderer.setSize($innerContainer.width(), $innerContainer.height());
 
-    animate();
+            requestAnimationFrame(animate);
+            controls.update();
+            render();
+        }
 
-    function animate() {
-        camera.aspect = $container.width() / $container.height();
-        camera.updateProjectionMatrix();
-        renderer.setSize($container.width(), $container.height());
+        function render() {
+            camera.lookAt(cameraTarget);
+            renderer.render(scene, camera);
+        }
 
-        requestAnimationFrame(animate);
-        controls.update();
-        render();
+        // Increment viewerCount
+        // This is currently only used for our fullscreen checkbox IDs
+        viewerCount++;
     }
 
-    function render() {
-        camera.lookAt(cameraTarget);
-        renderer.render(scene, camera);
-    }
-
+    // Helpers for computing object volumes
     function calculateVolume(object){
-        var total = 0;
+        let total = 0;
 
         geometry = new THREE.Geometry().fromBufferGeometry(object.geometry);
         faces = geometry.faces;
         vertices = geometry.vertices;
-        for(var i = 0; i < faces.length; i++){
-            var Pi = faces[i].a;
-            var Qi = faces[i].b;
-            var Ri = faces[i].c;
+        for(let i = 0; i < faces.length; i++){
+            let Pi = faces[i].a;
+            let Qi = faces[i].b;
+            let Ri = faces[i].c;
 
-            var P = new THREE.Vector3(vertices[Pi].x, vertices[Pi].y, vertices[Pi].z);
-            var Q = new THREE.Vector3(vertices[Qi].x, vertices[Qi].y, vertices[Qi].z);
-            var R = new THREE.Vector3(vertices[Ri].x, vertices[Ri].y, vertices[Ri].z);
+            let P = new THREE.Vector3(vertices[Pi].x, vertices[Pi].y, vertices[Pi].z);
+            let Q = new THREE.Vector3(vertices[Qi].x, vertices[Qi].y, vertices[Qi].z);
+            let R = new THREE.Vector3(vertices[Ri].x, vertices[Ri].y, vertices[Ri].z);
             total += signedVolumeOfTriangle(P, Q, R);
         }
         return Math.abs(total);
     }
 
     function signedVolumeOfTriangle(p1, p2, p3) {
-        var v321 = p3.x*p2.y*p1.z;
-        var v231 = p2.x*p3.y*p1.z;
-        var v312 = p3.x*p1.y*p2.z;
-        var v132 = p1.x*p3.y*p2.z;
-        var v213 = p2.x*p1.y*p3.z;
-        var v123 = p1.x*p2.y*p3.z;
+        let v321 = p3.x*p2.y*p1.z;
+        let v231 = p2.x*p3.y*p1.z;
+        let v312 = p3.x*p1.y*p2.z;
+        let v132 = p1.x*p3.y*p2.z;
+        let v213 = p2.x*p1.y*p3.z;
+        let v123 = p1.x*p2.y*p3.z;
         return (-v321 + v231 + v312 - v132 - v213 + v123) / 6.0;
     }
-}
+
+})();
