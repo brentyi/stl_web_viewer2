58bb4e39d9ffd15f5e8ec68144babdb34c1215ff
Brent Yi <yibrenth@gmail.com>
2019-11-26 00:47:23 -0800

    Add GitHub plug in fullscreen mode

--- scripts/STLWebViewer2.js
+++ scripts/STLWebViewer2.js
@@ -38,8 +38,12 @@
             <input class="stlwv2-fullscreen-checkbox" id="' + fullscreenCheckboxId + '" type="checkbox"></input>\
             <div class="stlwv2-inner">\
                 <div class="stlwv2-percent"></div>\
-                <label class="stlwv2-hud stlwv2-fullscreen-on" title="Fullscreen" for="' + fullscreenCheckboxId + '">&#x21F1;</label>\
-                <label class="stlwv2-hud stlwv2-fullscreen-off" title="Close" for="' + fullscreenCheckboxId + '">&times;</label>\
+                <label class="stlwv2-hud stlwv2-fullscreen-on" title="Fullscreen" for="' + fullscreenCheckboxId + '">\
+                    &#x21F1;</label>\
+                <label class="stlwv2-hud stlwv2-fullscreen-off" title="Close" for="' + fullscreenCheckboxId + '">\
+                    &times;</label>\
+                <a class="stlwv2-hud stlwv2-github-link" target="_blank" href="https://github.com/brentyi/stl_web_viewer2">\
+                    STL Web Viewer</a>\
             </div>\
         ');
         let $innerContainer = $container.children('.stlwv2-inner');
